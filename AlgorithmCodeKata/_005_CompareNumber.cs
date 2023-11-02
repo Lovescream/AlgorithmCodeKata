b478f75c409f42864899370bb3d91bedb6031a00
asd <114643897+Lovescream@users.noreply.github.com>
2023-11-02 15:14:54 +0900

    005

---
+++ AlgorithmCodeKata/_005_CompareNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata {
+    // 정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+    // 0 <= num1 <= 10,000
+    // 0 <= num2 <= 10,000
+    internal class _005_CompareNumber {
+
+        public int solution(int num1, int num2) {
+            return num1 == num2 ? 1 : -1;
+        }
+
+    }
+}
