ea547ef71a4face1354633c49775988b705e9a0f
asd <114643897+Lovescream@users.noreply.github.com>
2023-11-02 14:57:07 +0900

    1
    1

---
+++ AlgorithmCodeKata/_001_Subtract.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata {
+    // 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+    // -50000 ≤ num1 ≤ 50000
+    // -50000 ≤ num2 ≤ 50000
+
+    internal class _001_Subtract {
+        public int solution(int num1, int num2) {
+            return num1 - num2;
+        }
+    }
+}
