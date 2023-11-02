be014173b559473bc28446bd5642ea29d14a5382
asd <114643897+Lovescream@users.noreply.github.com>
2023-11-02 15:05:33 +0900

    003

---
+++ AlgorithmCodeKata/_003_Quotient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata {
+    // 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+    // 0 < num1 <= 100
+    // 0 < num2 <= 100
+    internal class _003_Quotient {
+        public int solution(int num1, int num2) {
+            return num1 / num2;
+        }
+
+    }
+}
