72fdf09914cfccc3e7decf6e34507cc2c7a5a5c9
asd <114643897+Lovescream@users.noreply.github.com>
2023-11-03 16:37:41 +0900

    017

---
+++ AlgorithmCodeKata/_017_ArrayReverse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata {
+    internal class _017_ArrayReverse {
+        public int[] solution(long n) {
+            string s = n.ToString();
+            int[] answer = new int[s.Length];
+            for (int i = 0; i < s.Length; i++) {
+                answer[i] = int.Parse(s[s.Length - i - 1].ToString());
+            }
+            return answer;
+        }
+    }
+}
