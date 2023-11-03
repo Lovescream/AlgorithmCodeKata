efaaf8896fa20e5b386edc49ded89b1a64621a96
asd <114643897+Lovescream@users.noreply.github.com>
2023-11-03 09:10:46 +0900

    006

---
+++ AlgorithmCodeKata/_006_AddNum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata {
+    // 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+    // -50,000 <= num1 <= 50,000
+    // -50,000 <= num2 <= 50,000
+    internal class _006_AddNum {
+
+        public int solution(int num1, int num2) => num1 + num2;
+
+    }
+}
