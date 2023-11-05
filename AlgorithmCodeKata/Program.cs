6bf03616484679d910c66552e41b2a85466e87be
asd <114643897+Lovescream@users.noreply.github.com>
2023-11-05 18:52:05 +0900

    023

--- AlgorithmCodeKata/Program.cs
+++ AlgorithmCodeKata/Program.cs
@@ -2,6 +2,8 @@
     internal class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello, World!");
+            _023_Collatz collatz = new();
+            collatz.solution(626331);
         }
     }
 }
