47a012c3c0f3facdaf2cb3b439a201fdb583c49b
asd <114643897+Lovescream@users.noreply.github.com>
2023-11-06 17:17:57 +0900

    049

--- AlgorithmCodeKata/Program.cs
+++ AlgorithmCodeKata/Program.cs
@@ -1,8 +1,13 @@
 ﻿namespace AlgorithmCodeKata {
     internal class Program {
         static void Main(string[] args) {
-            _048_KNumber sadas = new _048_KNumber();
-            sadas.solution(new int[] { 1, 5, 2, 6, 3, 7, 4 },4, 4);
+            _049_SelectTwoSum sadas = new _049_SelectTwoSum();
+            int[] a = sadas.solution(new int[] { 2, 1, 3, 4, 1 });
+            string s = "";
+            for (int i = 0; i < a.Length; i++) {
+                s += a[i].ToString();
+            }
+            Console.WriteLine(s);
         }
     }
 }
---
+++ AlgorithmCodeKata/_049_SelectTwoSum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata {
+    // 정수 배열 numbers가 주어집니다.
+    // numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+
+    // numbers의 길이는 2 이상 100 이하입니다.
+    // numbers의 모든 수는 0 이상 100 이하입니다.
+    internal class _049_SelectTwoSum {
+        private List<int> list = new List<int>();
+
+        public int[] solution(int[] numbers) {
+            Combine(numbers, 0, new List<int>());
+            return list.OrderBy(x => x).ToArray();
+        }
+
+        private void Combine(int[] nums, int index, List<int> selected) {
+            if (selected.Count == 2) {
+                int sum = selected[0] + selected[1];
+                if (!list.Contains(sum)) list.Add(sum);
+                return;
+            }
+            for (int i = index; i < nums.Length; i++) {
+                selected.Add(nums[i]);
+                Combine(nums, i + 1, selected);
+                selected.RemoveAt(selected.Count - 1);
+            }
+        }
+    }
+}
