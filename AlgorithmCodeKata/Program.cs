37456654aea40ce73a20428f3ba9c43e6e0e3718
asd <114643897+Lovescream@users.noreply.github.com>
2023-11-06 11:51:18 +0900

    034

--- AlgorithmCodeKata/Program.cs
+++ AlgorithmCodeKata/Program.cs
@@ -2,8 +2,8 @@
     internal class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello, World!");
-            _029_RemoveMin a = new();
-            a.solution(new int[] { 6, 5, 7, 2, 9 });
+            _034_StringDescending a = new();
+            a.solution("sadsafqkASDSADop");
         }
     }
 }
---
+++ AlgorithmCodeKata/_034_StringDescending.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata {
+    // 문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
+    // s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
+    internal class _034_StringDescending {
+        public string solution(string s) {
+            return new string(s.OrderByDescending(x => x).ToArray());
+        }
+    }
+}
