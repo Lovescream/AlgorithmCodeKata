cdab1a2cc7cce318c75d3a29a0a6b86d1b74be8e
asd <114643897+Lovescream@users.noreply.github.com>
2023-11-06 14:16:59 +0900

    043

--- AlgorithmCodeKata/Program.cs
+++ AlgorithmCodeKata/Program.cs
@@ -1,8 +1,9 @@
 ﻿namespace AlgorithmCodeKata {
     internal class Program {
-        //static void Main(string[] args) {
-        //    Console.WriteLine("Hello, World!");
-        //    _038_RectangularStar.Main();
-        //}
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+            _043_UndersizedSubstring a = new _043_UndersizedSubstring();
+            a.solution("131421451", "221424");
+        }
     }
 }
