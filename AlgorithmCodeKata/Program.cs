523630f02edb17f66377f3a899d8b9412401af21
asd <114643897+Lovescream@users.noreply.github.com>
2023-11-06 16:59:52 +0900

    048

--- AlgorithmCodeKata/Program.cs
+++ AlgorithmCodeKata/Program.cs
@@ -1,8 +1,8 @@
 ﻿namespace AlgorithmCodeKata {
     internal class Program {
         static void Main(string[] args) {
-            _045_CaesarCipher sadas = new _045_CaesarCipher();
-            sadas.Temp();
+            _048_KNumber sadas = new _048_KNumber();
+            sadas.solution(new int[] { 1, 5, 2, 6, 3, 7, 4 },4, 4);
         }
     }
 }
