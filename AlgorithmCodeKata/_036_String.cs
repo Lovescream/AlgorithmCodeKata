affff6dfc6cb2336db26099d95be14d4d337e392
asd <114643897+Lovescream@users.noreply.github.com>
2023-11-06 12:22:50 +0900

    036

---
+++ AlgorithmCodeKata/_036_String.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata {
+    internal class _036_String {
+        public bool solution(string s) {
+            if (s.Length != 4 && s.Length != 6) return false;
+            if (long.TryParse(s, out long temp)) return true;
+            return false;
+        }
+    }
+}
