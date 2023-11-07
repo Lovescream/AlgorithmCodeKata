423f092ca130d131115756cb3fce2c1d724b7cc9
asd <114643897+Lovescream@users.noreply.github.com>
2023-11-07 21:03:26 +0900

    051

--- AlgorithmCodeKata/Program.cs
+++ AlgorithmCodeKata/Program.cs
@@ -1,12 +1,7 @@
 ﻿namespace AlgorithmCodeKata {
     internal class Program {
         static void Main(string[] args) {
-            _050_NearestEquals sadas = new _050_NearestEquals();
-            int[] a = sadas.solution("banana");
-            string s = "";
-            for (int i = 0; i < a.Length; i++) {
-                s += a[i].ToString();
-            }
+            string s = new _051_FoodFight().solution(new int[] { 1, 3, 4, 6 });
             Console.WriteLine(s);
         }
     }
