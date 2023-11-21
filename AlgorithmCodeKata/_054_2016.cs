a36aa5523faf69518d54770dcaad7f11a2f08189
asd <114643897+Lovescream@users.noreply.github.com>
2023-11-21 10:30:33 +0900

    054

---
+++ AlgorithmCodeKata/_054_2016.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata {
+    public class _054_2016 {
+        public string solution(int a, int b) => new DateTime(2016, a, b).ToString("ddd").ToUpper();
+    }
+}
