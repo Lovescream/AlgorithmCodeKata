23fdbf4b67b247dc7172175762a6610c5dfb7d17
asd <114643897+Lovescream@users.noreply.github.com>
2023-11-24 10:55:18 +0900

    057

--- AlgorithmCodeKata/Program.cs
+++ AlgorithmCodeKata/Program.cs
@@ -1,10 +1,13 @@
 ﻿namespace AlgorithmCodeKata {
     public class Program {
         static void Main(string[] args) {
-            _056_Fruiterer fruiterer = new _056_Fruiterer();
-            Console.WriteLine(fruiterer.solution(3, 4, new int[] { 1, 2, 3, 1, 2, 3, 1 }));
-            Console.WriteLine(fruiterer.solution(4, 3, new int[] { 4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2 }));
-
+            //_056_Fruiterer fruiterer = new _056_Fruiterer();
+            //Console.WriteLine(fruiterer.solution(3, 4, new int[] { 1, 2, 3, 1, 2, 3, 1 }));
+            //Console.WriteLine(fruiterer.solution(4, 3, new int[] { 4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2 }));
+            _057_MockTest test = new _057_MockTest();
+            PrintArray(test.solution(new int[] { 1, 2, 3, 4, 5 }));
+            PrintArray(test.solution(new int[] { 1, 3, 2, 4, 2 }));
+            
         }
 
         public static void PrintArray(int[] array) {
