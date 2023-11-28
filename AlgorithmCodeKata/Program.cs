2de37a3ce17aaceac5768bbed286b2e346980b7c
asd <114643897+Lovescream@users.noreply.github.com>
2023-11-28 09:40:07 +0900

    058

--- AlgorithmCodeKata/Program.cs
+++ AlgorithmCodeKata/Program.cs
@@ -4,10 +4,12 @@
             //_056_Fruiterer fruiterer = new _056_Fruiterer();
             //Console.WriteLine(fruiterer.solution(3, 4, new int[] { 1, 2, 3, 1, 2, 3, 1 }));
             //Console.WriteLine(fruiterer.solution(4, 3, new int[] { 4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2 }));
-            _057_MockTest test = new _057_MockTest();
-            PrintArray(test.solution(new int[] { 1, 2, 3, 4, 5 }));
-            PrintArray(test.solution(new int[] { 1, 3, 2, 4, 2 }));
-            
+            //_057_MockTest test = new _057_MockTest();
+            //PrintArray(test.solution(new int[] { 1, 2, 3, 4, 5 }));
+            //PrintArray(test.solution(new int[] { 1, 3, 2, 4, 2 }));
+            _058_GetPrime prime = new _058_GetPrime();
+            Console.WriteLine(prime.solution(new int[] { 1, 2, 3, 4 }));
+            Console.WriteLine(prime.solution(new int[] { 1, 2, 7, 6, 4 }));
         }
 
         public static void PrintArray(int[] array) {
---
+++ AlgorithmCodeKata/_058_GetPrime.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata {
+    public class _058_GetPrime {
+        public int solution(int[] nums) {
+            int answer = 0;
+            for (int i = 0; i < nums.Length; i++) {
+                for (int j = i + 1; j < nums.Length; j++) {
+                    for (int k = j + 1; k < nums.Length; k++) {
+                        int sum = nums[i] + nums[j] + nums[k];
+                        if (IsPrime(sum)) answer++;
+                    }
+                }
+            }
+
+            return answer;
+        }
+        private bool IsPrime(int num) {
+            if (num <= 1) return false;
+            for (int i = 2; i * i <= num; i++) {
+                if (num % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
