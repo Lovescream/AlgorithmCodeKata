5d0f4bd6948b5b1f29e6afd9884d1dd9b10f4932
asd <114643897+Lovescream@users.noreply.github.com>
2023-12-11 10:02:47 +0900

    064

--- AlgorithmCodeKata/Program.cs
+++ AlgorithmCodeKata/Program.cs
@@ -1,8 +1,11 @@
 ﻿namespace AlgorithmCodeKata {
     public class Program {
         static void Main(string[] args) {
-            var a = new _063_NumberPartner();
-            Console.WriteLine(a.solution("3403", "13203"));
+            var a = new _064_SweatSuits();
+            Console.WriteLine(a.solution(5, new int[] { 2, 4 }, new int[] { 5, 3 }));
+            Console.WriteLine(a.solution(5, new int[] { 2, 4 }, new int[] { 3 }));
+            Console.WriteLine(a.solution(3, new int[] { 3 }, new int[] { 1 }));
+            Console.WriteLine(a.solution(5, new int[] { 1, 5 }, new int[] { 3 }));
         }
 
         public static void PrintArray(int[] array) {
